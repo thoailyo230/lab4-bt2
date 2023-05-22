4c36fb56b577296351dfda0787fec32a0c76077b
thoailyo230 <125753220+thoailyo230@users.noreply.github.com>
2023-05-22 16:46:58 +0700

    Create khachhangvietnam.java

---
+++ khachhangvietnam.java
@@ -0,0 +1,6 @@
+class VietnameseCustomer extends Customer {
+
+    public VietnameseCustomer(String id, String name, String date, String type, double quantity, double unitPrice, double threshold) {
+        super(id, name, date, type, quantity, unitPrice);
+    }
+}
